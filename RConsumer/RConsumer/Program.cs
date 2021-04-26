e41f9dad432945af1c2005ddb731b414517faf90
Andrei Gustavo <andrei.gustavo96@gmail.com>
2021-04-25 22:32:51 -0300

    Configurando RabbitMQ para publicacao

--- RConsumer/RConsumer/Program.cs
+++ RConsumer/RConsumer/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace RConsumer
 {
@@ -6,7 +9,59 @@ namespace RConsumer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Criar conexao com o rabbitMQ
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+            using var connection = factory.CreateConnection();
+            using (var channel = connection.CreateModel())
+            {
+                // Criar a fila (não é necessário criar no código)
+                // dica: Criar a fila no startUp e não dentro do código da regra de negócio
+                channel.QueueDeclare(
+                    queue: "queue_produtos",
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null
+                );
+                
+                // Consumidor
+                var consumer = new EventingBasicConsumer(channel);
+                
+                // Received  é chamado toda vez que a mensagem chegar na fila com 
+                // os dois parametros "model" e "ea"
+                consumer.Received += (model, ea) =>
+                {
+                    try
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] Received [0]", message);
+                        
+                        channel.BasicAck(ea.DeliveryTag, false);
+                    }
+                    catch (Exception ex)
+                    {
+                        // toda vez que houver uma exceção, ele vai retornar um NAK
+                        // e devolver a mensagem para fila
+                        Console.WriteLine($"Exception: {ex}.");
+                        channel.BasicNack(ea.DeliveryTag, false, true);
+                    }
+                };
+                
+                // Configurações do consumer (qual fila ele irá consumir)
+                channel.BasicConsume(
+                    queue: "queue_produtos",
+                    // autoAck = true (toda vez que obter um item da fila ele vai
+                    // retornar um AK)
+                    autoAck: false,
+                    consumer: consumer
+                );
+                
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
         }
     }
 }
--- RProducer/RProducer/Program.cs
+++ RProducer/RProducer/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using RabbitMQ.Client;
 
 namespace RProducer
 {
@@ -6,7 +8,42 @@ namespace RProducer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Criar conexao com o rabbitMQ
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+            using var connection = factory.CreateConnection();
+            
+            // Ativar vários canais através da mesma conexão
+            using (var channel = connection.CreateModel())
+            {
+                // Criar a fila (não é necessário criar no código)
+                // dica: Criar a fila no startUp e não dentro do código da regra de negócio
+                channel.QueueDeclare(
+                    queue: "queue_produtos",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null
+                );
+
+                string message = "Produto Teste";
+                var body = Encoding.UTF8.GetBytes(message);
+                
+                // Publicação da mensagem
+                channel.BasicPublish(
+                    exchange: "",
+                    routingKey: "queue_produtos",
+                    basicProperties: null,
+                    body: body
+                );
+                
+                Console.Write("[x] Sent {0}", message);
+            }
+            
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
         }
     }
 }
