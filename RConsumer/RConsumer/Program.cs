323a61605d831d9abed55b23a1f2a9249825708b
Andrei Gustavo <andrei.gustavo96@gmail.com>
2021-04-25 21:57:17 -0300

    Starting with RConsumer

---
+++ RConsumer/RConsumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
